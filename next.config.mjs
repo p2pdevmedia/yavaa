afbc7343cb51ae9ef009ab267b7587aeba285f61
p2pdevmedia <p2pdev@protonmail.com>
2026-05-08 22:25:53 -0300

    etapa 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
+  outputFileTracingIncludes: {
+    '/supabase': ['./node_modules/.prisma/client/**/*']
+  },
   turbopack: {
     root: process.cwd()
   }
